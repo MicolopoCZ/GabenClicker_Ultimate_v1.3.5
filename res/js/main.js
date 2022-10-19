754bad6f3bc0e12a40fcaddbad7d5dc6b2ebd7be
MicolopoCZ <armymisa@gmail.com>
2022-10-19 08:36:46 +0200

    first commit

---
+++ res/js/main.js
@@ -0,0 +1,43 @@
+const cookie = document.getElementById("cookie");
+const counter = document.getElementById("counter");
+const clickUpgrade = document.getElementById("clickUpgrade")
+const autoclickerUpgrade = document.getElementById("autoclickerUpgrade")
+
+let cookies = 0;
+
+let clickIncrease = 1;
+let clickIncreasePrice = 100;
+
+let autoclickerIncrease = 0;
+let autoclickerPrice = 200;
+
+cookie.onclick = () => {
+    cookies += clickIncrease;
+    counter.innerHTML = cookies;
+}
+
+clickUpgrade.onclick = () => {
+    if (cookies >= clickIncreasePrice) {
+        cookies -= clickIncreasePrice;
+        clickIncreasePrice += 100;
+        clickUpgrade.innerHTML = `Buy Click Upgrade: ${clickIncreasePrice} dollars`
+        counter.innerHTML = cookies;
+        clickIncrease++;
+    }
+}
+
+autoclickerUpgrade.onclick = () => {
+    if (cookies >= autoclickerPrice) {
+        cookies -= autoclickerPrice;
+        counter.innerHTML = cookies;
+        autoclickerPrice += 100;
+        autoclickerUpgrade.innerHTML = `Buy Autoclicker: ${autoclickerPrice} dollars`
+        if (autoclickerIncrease == 0) {
+            setInterval(() => {
+                cookies += autoclickerIncrease;
+                counter.innerHTML = cookies;
+            }, 1000);
+        }
+        autoclickerIncrease++;
+    }
+}
