e7cd2831cc82fe7cee4f421ae4c2c2fd2170b795
Michal Růžička <115612586+MicolopoCZ@users.noreply.github.com>
2022-12-14 09:14:59 +0100

    Add files via upload

--- res/js/csgo.js
+++ res/js/csgo.js
@@ -10,11 +10,42 @@ const info = document.getElementById("info");
 const kalasSFX = document.getElementById("fireSFX");
 const pnineSFX = document.getElementById("enemyfireSFX");
 
+
+// DMG zbraní
+const akDMG = 36;
+const pDMG = 25;
+
+// DMG zbraní proti kevlaru
+const akDMGA = 27;
+const pDMGA = 17;
+
+
+// hráčova střelba
 attack.onmousedown = () => {
-    if (enemyHp.innerHTML > 0 && playerHp.innerHTML > 0) {
-        enemyHp.innerHTML --;
-        enemyHp.style.color = "red";
-        enemyHp.style.fontSize = "50px";
+    let firing = hitChance[Math.floor(Math.random() * hitChance.length)];
+
+    if (firing == 1) {
+        if (enemyHp.innerHTML > 0 && playerHp.innerHTML > 0) {
+            enemyHp.innerHTML = enemyHp.innerHTML - akDMGA;
+            enemyHp.style.color = "red";
+            enemyHp.style.fontSize = "50px";
+            player.style.display = "none";
+            playerAttack.style.display = "flex"
+            playerAttack.style.display = "flex";
+            kalasSFX.play();
+            kalasSFX.volume = 1.5;
+            kalasSFX.currentTime = 0,5;
+        }
+
+        if (enemyHp.innerHTML <= 0) {
+            clearInterval(enemyAttackInterval);
+            enemyHp.innerHTML = "0";
+            info.innerHTML = "Terroristé vyhráli";
+            info.style.display = "block";
+        }
+    }
+    
+    if (firing == 0) {
         player.style.display = "none";
         playerAttack.style.display = "flex"
         playerAttack.style.display = "flex";
@@ -22,11 +53,6 @@ attack.onmousedown = () => {
         kalasSFX.volume = 1.5;
         kalasSFX.currentTime = 0,5;
     }
-    if (enemyHp.innerHTML == 0) {
-        info.innerHTML = "Info: Nepřítel zemřel";
-        clearInterval(enemyAttackInterval);
-    }
-    
 };
 
 attack.onmouseup = () => {
@@ -36,26 +62,38 @@ attack.onmouseup = () => {
     enemyHp.style.fontSize = "50px";
 };
 
-    heal.onclick = () => {
-        if (playerHp.innerHTML > 0) {
-        playerHp.innerHTML = "20";
-    };
-}
 
+// NPC střelba
 const enemyAttackInterval = setInterval(() => {
     if (playerHp.innerHTML <= 0) {
         clearInterval(enemyAttackInterval);
-        info.innerHTML = "Info: Hráč zemřel"
+        info.innerHTML = "Counter Terroristé vyhráli"
     }
-    playerHp.innerHTML --;
-    enemy.style.display = "none";
-    enemyAttack.style.display = "flex";
-    pnineSFX.play();
-    pnineSFX.volume = 0.01;
-    pnineSFX.currentTime = 0;
-}, 800);
+    let firing = hitChance[Math.floor(Math.random() * hitChance.length)];
+    if (firing == 1) {
+        playerHp.innerHTML = playerHp.innerHTML - pDMGA;
+        enemy.style.display = "none";
+        enemyAttack.style.display = "flex";
+        pnineSFX.play();
+        pnineSFX.volume = 0.01;
+        pnineSFX.currentTime = 0;
+    }
+    if (firing == 0) {
+        enemy.style.display = "none";
+        enemyAttack.style.display = "flex";
+        pnineSFX.play();
+        pnineSFX.volume = 0.01;
+        pnineSFX.currentTime = 0;
+    }
+}, 300);
 
-const enemyMoveInterval = setInterval(() => {
+const enemyFireInterval = setInterval(() => {
     enemy.style.display = "flex";
     enemyAttack.style.display = "none";
-}, 900);
+}, 400);
+
+
+// šance na hit
+let hitChance = [
+    0, 0, 0, 0, 0, 0, 1, 1, 1
+];
